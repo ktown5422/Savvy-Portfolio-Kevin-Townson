14296b7b8f4f1da5725c8e57d72348a9ad496aad
Kevin Townson <ktown5422@gmail.com>
2019-02-22 00:14:25 -0600

    changed the document query and took out console log function

--- index.js
+++ index.js
@@ -2,66 +2,75 @@ import Navigation from './src/Navigation';
 import Header from './src/Header';
 import Content from './src/Content';
 import Footer from './src/Footer';
-import nameChecker from './src/Greeter';
+// import pickName from './src/Greeter';
 
+// Create a STATE object
 var home = {
-    'title': 'Welcome to my website'
+    'title': 'Welcome to My Saavy Coders website!'
 };
 
 var blog = {
-    'title': 'Welcome to my blog'
+    'title': 'Check out the Blogs'
 };
 
-var contact = {
-    'title': 'Welcome to my contact'
+var apply = {
+    'title': 'Please apply for something'
 };
 
 var projects = {
-    'title': 'Welcome to my projects'
+    'title': 'These projects boii!'
 };
 
 var root = document.querySelector('#root');
 
+// Create a rendering function //
 function render(state){
-root.innerHTML = `
-${Navigation(state)}
-${Header(state.title)}
-${Content(state)}
-${Footer(state)}
-
-`;
+    root.innerHTML = ${Navigation(state)}
+    ${Header(state.title)}
+    ${Content(state)}
+    ${Footer(state)};
 }
 
 render(home);
 
-
-document.querySelector('#navigation li:nth-child(1) > a')
-.addEventListener('click', (e) => {
-    e.preventDefault ();
+document.querySelector('#navigation li:first-child').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e.target.href);
     render(home);
 });
 
-document.querySelector('#navigation li:nth-child(2) > a')
-.addEventListener('click', (e) => {
-    e.preventDefault ();
+document.querySelector('#navigation li:nth-child(2)').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e.target.href);
     render(blog);
-    
 });
 
-document.querySelector('#navigation li:nth-child(3) > a')
-.addEventListener('click', (e) => {
-    e.preventDefault ();
-    render(contact);
-    
+document.querySelector('#navigation li:nth-child(3)').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e.target.href);
+    render(apply);
 });
 
-
-document.querySelector('#navigation li:nth-child(4) > a')
-.addEventListener('click', (e) => {
-    e.preventDefault ();
+document.querySelector('#navigation li:nth-child(4)').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e.target.href);
     render(projects);
-    
 });
 
-
-
+// Username input function
+// pickName();
+console.log(e.target.textContent);
+    console.log(e.target.outerHTML);
+    console.log(e.innerHTML);
+    console.log(e.target.href);
+    render(home);
+console.log(e.target.textContent);
+    console.log(e.target.outerHTML);
+    console.log(e.innerHTML);
+    console.log(e.target.href);
+    render(home);
+console.log(e.target.textContent);
+    console.log(e.target.outerHTML);
+    console.log(e.innerHTML);
+    console.log(e.target.href);
+    render(home);
