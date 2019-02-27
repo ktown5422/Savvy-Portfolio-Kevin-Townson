d6bdc0228cf71d9752543fadb0113f91e0b678c3
Kevin Townson <ktown5422@gmail.com>
2019-02-26 20:26:04 -0600

    add render states on links

--- index.js
+++ index.js
@@ -2,75 +2,96 @@ import Navigation from './src/Navigation';
 import Header from './src/Header';
 import Content from './src/Content';
 import Footer from './src/Footer';
-// import pickName from './src/Greeter';
+import nameChecker from './src/Greeter';
 
-// Create a STATE object
-var home = {
-    'title': 'Welcome to My Saavy Coders website!'
-};
 
-var blog = {
-    'title': 'Check out the Blogs'
-};
 
-var apply = {
-    'title': 'Please apply for something'
-};
 
-var projects = {
-    'title': 'These projects boii!'
+var State = { 
+    'Home':  {
+        'title': 'Welcome to my Website'
+},
+
+    'Blog':  {
+        'title': 'Welcome to my Blog'
+},
+
+    'Contact':  {
+        'title': 'Welcome to my Contact'
+},
+
+    'Projects':  {
+        'title': 'Welcome to my Projects'
+}
 };
 
 var root = document.querySelector('#root');
 
-// Create a rendering function //
 function render(state){
-    root.innerHTML = ${Navigation(state)}
+    var links;
+    
+    root.innerHTML = `
+    ${Navigation(state)}
     ${Header(state.title)}
     ${Content(state)}
-    ${Footer(state)};
+    ${Footer(state)}
+    `;
+    
+    links = document.querySelectorAll('#navigation > ul > li > a'); 
+
+    links[0].addEventListener('click', (event) => {
+        event.preventDefault();
+        render(State[event.target.textContent]);
+    });
+
+    links[1].addEventListener('click', (event) => {
+        event.preventDefault();
+        render(State[event.target.textContent]);
+    });
+
+    links[2].addEventListener('click', (event) => {
+        event.preventDefault();
+        render(State[event.target.textContent]);
+    });
+
+    links[3].addEventListener('click', (event) => {
+        event.preventDefault();
+        render(State[event.target.textContent]);
+    });
 }
 
-render(home);
+render(State.Home);
 
-document.querySelector('#navigation li:first-child').addEventListener('click', (e) => {
-    e.preventDefault();
+ /*document.querySelector('#navigation li:first-child')
+.addEventListener('click', (e) => {
+    e.preventDefault ();
     console.log(e.target.href);
     render(home);
 });
 
-document.querySelector('#navigation li:nth-child(2)').addEventListener('click', (e) => {
-    e.preventDefault();
+document.querySelector('#navigation li:nth-child(2) > a')
+.addEventListener('click', (e) => {
+    e.preventDefault ();
     console.log(e.target.href);
     render(blog);
+    
 });
 
-document.querySelector('#navigation li:nth-child(3)').addEventListener('click', (e) => {
-    e.preventDefault();
+document.querySelector('#navigation li:nth-child(3) > a')
+.addEventListener('click', (e) => {
+    e.preventDefault ();
     console.log(e.target.href);
-    render(apply);
+    render(contact);
+    
 });
 
-document.querySelector('#navigation li:nth-child(4)').addEventListener('click', (e) => {
-    e.preventDefault();
+
+document.querySelector('#navigation li:nth-child(4) > a')
+.addEventListener('click', (e) => {
+    e.preventDefault ();
     console.log(e.target.href);
     render(projects);
-});
+    
+});*/
 
-// Username input function
-// pickName();
-console.log(e.target.textContent);
-    console.log(e.target.outerHTML);
-    console.log(e.innerHTML);
-    console.log(e.target.href);
-    render(home);
-console.log(e.target.textContent);
-    console.log(e.target.outerHTML);
-    console.log(e.innerHTML);
-    console.log(e.target.href);
-    render(home);
-console.log(e.target.textContent);
-    console.log(e.target.outerHTML);
-    console.log(e.innerHTML);
-    console.log(e.target.href);
-    render(home);
+// nameChecker();
