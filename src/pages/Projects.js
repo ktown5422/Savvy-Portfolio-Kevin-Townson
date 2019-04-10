1d8d1e974ae6246e98a8ced7b20cd81668564d32
Kevin Townson <ktown5422@gmail.com>
2019-04-10 05:32:47 -0500

    add card to projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,21 @@
 export default function Projects(){
     return `
     <h3>Check Out My Work</h3>
+    <br>
+    <div class="card">
+        <img src="" class="card-image">
+        <div class="card-footer">
+            <div class="card-title">
+                MeMusic
+            </div>
+            <div class="card-description">
+            MeMusic will consist of music from artist all over the world using spotify’s web API.. Users will have access to Search Artist  and access the playlist of their favorite music. I managed the tasks of my project using trello. MeMusic was built using Javascript, HTML/CSS, JSON, Spotify’s Web API and Adobe XD for wireframing.
+
+            </div>
+            <a class="card-button" href="https://memusic.netlify.com/">
+                View MeMusic
+            </a>
+        </div>
+    </div>
     `;
 }
